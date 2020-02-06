fixes - fixed bug in vocab creation when token is already in special tokens - added some asserts - added some tqdm - changed the way tokenizer.numericalize works

get_vocab.py
@@ -11,10 +11,12 @@ def get_freqs(path, fields):
     being a counter for the frequency of each token
     """
 
+    assert isinstance(fields, (list, tuple))
+
     freqs = defaultdict(Counter)
 
     with open(path, 'r') as f:
-        for line in tqdm(f):
+        for line in tqdm(f, desc='Building vocab frequencies...'):
             example = json.loads(line)
             for field in fields:
                 freqs[field].update(example[field])
@@ -22,17 +24,17 @@ def get_freqs(path, fields):
     return freqs
 
 
-amazon_freqs = get_freqs('data/amazon_train.jsonl', ['tokens', 'tags'])
+yelp_freqs = get_freqs('data/yelp_train.jsonl', ['tokens', 'tags'])
 
-amazon_tokens_vocab = Vocab(amazon_freqs['tokens'], max_size=25_000,
-                            min_freq=1, special_tokens=['<sos>', '<eos>'])
+yelp_tokens_vocab = Vocab(yelp_freqs['tokens'], max_size=25_000,
+                          min_freq=2, special_tokens=['<sos>', '<eos>'])
 
-amazon_tags_vocab = Vocab(amazon_freqs['tags'], unk_token=None,
-                          special_tokens=['<sos>', '<eos>'])
+yelp_tags_vocab = Vocab(yelp_freqs['tags'], unk_token=None,
+                        special_tokens=['<sos>', '<eos>'])
 
 tokenizer = torch.load('tokenizer_novocab.pt')
 
-tokenizer.vocabs['tokens'] = amazon_tokens_vocab
-tokenizer.vocabs['tags'] = amazon_tags_vocab
+tokenizer.vocabs['tokens'] = yelp_tokens_vocab
+tokenizer.vocabs['tags'] = yelp_tags_vocab
 
 tokenizer = torch.save(tokenizer, 'tokenizer.pt')