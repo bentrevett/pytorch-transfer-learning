1d7c9539dbb1a818a342b93a7a7e1f370cc13ebc
bentrevett <bentrevett@gmail.com>
2020-02-06 19:50:10 +0000

    fixes + added missing .py extensions to run script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-python predict_sentiment --name predict_sentiment_no_pretrain
-python predict_tags --name predict_tag
-python predict_mask --name predict_mask
-python predict_sentiment --name predict_sentiment_tag_pretrain --load checkpoints/model-predict_tag
-python predict_sentiment --name predict_sentiment_mask_pretrain --load checkpoints/model-predict_mask
+python predict_sentiment.py --name predict_sentiment_no_pretrain
+python predict_tags.py --name predict_tag
+python predict_mask.py --name predict_mask
+python predict_sentiment.py --name predict_sentiment_tag_pretrain --load checkpoints/model-predict_tag
+python predict_sentiment.py --name predict_sentiment_mask_pretrain --load checkpoints/model-predict_mask
